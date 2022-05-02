e57b20878cc40bb0ad34a90d643146909c82d690
Marcin Sęk <marcin.sek123@gmail.com>
2022-05-02 20:03:47 +0200

    8.4. Obsługiwanie błędów na różnych przypadkach, Zadanie: wiedza o wyjątkach/poprawka v2

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -12,12 +12,13 @@ public class FlightFinder {
 
     public Boolean findFlight(Flight flight) throws RouteNotFoundException {
 
-        if (timetable.get(flight.getArrivalAirport()) == null) {
+        Boolean result = timetable.get(flight.getArrivalAirport());
+        if (result == null) {
             throw new RouteNotFoundException("ERROR: Arrival airport is not available");
-        } else if (timetable.get(flight.getArrivalAirport())) {
+        } else if (result) {
             System.out.println(flight.getArrivalAirport() + " is on our list ");
         } else
             System.out.println(flight.getArrivalAirport() + " is not on our list ");
-        return null;
+        return result;
     }
 }
