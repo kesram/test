cf66567aa2c88bdb57017e15e6bb5750c9ae2e96
Marcin Sęk <marcin.sek123@gmail.com>
2022-05-10 08:13:13 +0200

    SOLID i DRY

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/CarRentalService.java
@@ -0,0 +1,25 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.time.LocalDateTime;
+
+public class CarRentalService {
+    public boolean rent(final User user, final LocalDateTime carRentFrom, final LocalDateTime carRentTo) {
+        System.out.println("Renting Car for: " + user.getName() + user.getSurname()
+                + " from: " + carRentFrom.toString() + " to: " + carRentTo.toString());
+
+        MailService mailService = new MailService();
+        mailService.sendEmail(user);
+
+        CarRentalRepository carRentalRepository = new CarRentalRepository();
+        carRentalRepository.createCarRental(user, carRentFrom, carRentTo);
+
+        return true;
+    }
+
+    public boolean rent(final User user, final LocalDateTime carRentFrom, final LocalDateTime carRentTo) {
+        System.out.println("Renting Car for: " + user.getName() + user.getSurname()
+                + " from: " + carRentFrom.toString() + " to: " + carRentTo.toString());
+
+        return true;
+    }
+}
