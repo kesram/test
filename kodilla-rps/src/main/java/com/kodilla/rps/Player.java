69a261cb370622444593eb5207e19f4cfce0d530
Marcin Sęk <marcin.sek123@gmail.com>
2022-05-23 13:28:37 +0200

    RPS, Zadanie: gra w papier, kamień, nożyce

--- kodilla-rps/src/main/java/com/kodilla/rps/Player.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/Player.java
@@ -4,33 +4,11 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Player {
-    private String playerName;
     final Scanner scanner = new Scanner(System.in);
-    private boolean end;
     private int playerPoints;
     private int compPoints;
     private int maxScore;
-    private String nowaGra;
-
-    public String getNowaGra() {
-        nowaGra = scanner.nextLine();
-        return nowaGra;
-    }
-
-    public void isItEnd(){
-        if(playerPoints == maxScore){
-            System.out.println("Gracz wygrał. Koniec Gry"); // dodać "Gramy od nowa? Y/N"
-        } else if(compPoints == maxScore) {
-            System.out.println("Komputer wygrał. Koniec gry"); // dodać "Gramy od nowa? Y/N"
-        } else System.out.println("Gramy dalej");
-    }
-
-    public int getCompPoints() {
-        return compPoints;
-    }
-    public int getPlayerPoints() {
-        return playerPoints;
-    }
+    private boolean end;
 
     final String[] moves = new String[3];{
         moves[0] = "Kamień";
@@ -38,82 +16,122 @@ public class Player {
         moves[2] = "Nożyce";
     }
 
-    public Player() {
+    public boolean getEnd(){
+        return end;
     }
 
-
-    public void getName(){
+    public void getName() {
         System.out.println("Wprowadź imię: ");
-        String player = scanner.nextLine();
-        System.out.println("Wprowadzone imię to: "+ player);
+        String playerName = scanner.nextLine();
+        System.out.println("Wprowadzone imię to: " + playerName);
     }
 
-    public int getMaxScore(){
+    public int getMaxScore() {
         System.out.println("Do ilu chcesz zagrać?");
-        maxScore = scanner.nextInt();
+        String score = scanner.nextLine();
+        maxScore = Integer.parseInt(score);
         return maxScore;
     }
 
-    public void gameResult(){
+    public void gameResult () {
         System.out.println("Punkty komputera: " + compPoints);
         System.out.println("Punkty gracza: " + playerPoints);
     }
 
-    public void generateMove(){
-        System.out.println("Proszę wykonj swój ruch:");
-        int move = scanner.nextInt();
-        Random random = new Random();
-        int compResult =random.nextInt(3) +1;
+    public int getCompPoints () {
+        return compPoints;
+    }
 
-        if(compResult == 1 && move == 1) {
+    public int getPlayerPoints () {
+        return playerPoints;
+    }
+
+    public void isItEnd () {
+        if (playerPoints == maxScore) {
+            System.out.println("Gracz wygrał. Koniec Gry");
+            end = true;
+        } else if (compPoints == maxScore) {
+            System.out.println("Komputer wygrał. Koniec gry");
+            end = true;
+        } else System.out.println(" ");
+    }
+
+    public void generateMove() {
+        System.out.println("Instrukcja: 1-kamień, 2-papier, 3-nożyce, x-koniec gry, n-nowa gra");
+        System.out.println("Wykonj swój ruch:");
+        String input = scanner.nextLine().toUpperCase();
+        Random random = new Random();
+        int compResult = random.nextInt(3) + 1;
+
+        String yesNo;
+        if (input.equals("N")) {
+            System.out.println("Nowa gra. Potwierdz TAK/NIE");
+            yesNo = scanner.nextLine().toUpperCase();
+            if (yesNo.equals("TAK")) {
+                System.out.println("zaczynamy nową grę");
+                playerPoints = 0;
+                compPoints = 0;
+            } else if (yesNo.equals("NIE")) {
+                System.out.println("Gramy dalej ");
+            } else System.out.println("nie ma takiego symbolu");
+        } else if (input.equals("X")) {
+            System.out.println("Kończymy grę? Potwierdz TAK/NIE");
+            yesNo = scanner.nextLine().toUpperCase();
+            if (yesNo.equals("TAK")) {
+                System.out.println("Kończymy grę");
+                end = true;
+            } else if (yesNo.equals("NIE")) {
+                System.out.println("Gramy dalej");
+            } else System.out.println("nie ma takiego symbolu");
+        } else if (compResult == 1 && input.equals("1")) {
             System.out.println("Ruch komputera to " + moves[0]);
             System.out.println("Twój ruch to " + moves[0]);
             System.out.println("remis - punkt dla Komputera");
             compPoints += 1;
             gameResult();
-        } else if(compResult == 1 && move == 3){
+        } else if (compResult == 1 && input.equals("3")) {
             System.out.println("Ruch komputera to " + moves[0]);
             System.out.println("Twój ruch to " + moves[2]);
             System.out.println("Kamień bije nożyce - punkt dla Komputera");
             compPoints += 1;
             gameResult();
-        } else if(compResult == 1 && move == 2) {
+        } else if (compResult == 1 && input.equals("2")) {
             System.out.println("Ruch komputera to " + moves[0]);
             System.out.println("Twój ruch to " + moves[1]);
             System.out.println("Kamień przegrywa z papierem - punkt dla Gracza");
             playerPoints += 1;
             gameResult();
-        } else if(compResult == 3 && move == 1){
+        } else if (compResult == 3 && input.equals("1")) {
             System.out.println("Ruch komputera to " + moves[2]);
             System.out.println("Twój ruch to " + moves[0]);
             System.out.println("Kamień bije nożyce - punkt dla Gracza");
             playerPoints += 1;
             gameResult();
-        } else if(compResult == 2 && move == 1) {
+        } else if (compResult == 2 && input.equals("1")) {
             System.out.println("Ruch komputera to " + moves[1]);
             System.out.println("Twój ruch to " + moves[0]);
             System.out.println("Kamień przegrywa z papierem - punkt dla Komputera");
             compPoints += 1;
             gameResult();
-        } else if (compResult == 2 && move == 2){
+        } else if (compResult == 2 && input.equals("2")) {
             System.out.println("Ruch komputera to " + moves[1]);
             System.out.println("Twój ruch to " + moves[1]);
             System.out.println("remis - punkt dla Komputera");
             compPoints += 1;
             gameResult();
-        } else if(compResult == 3 && move == 3){
+        } else if (compResult == 3 && input.equals("3")) {
             System.out.println("Ruch komputera to " + moves[2]);
             System.out.println("Twój ruch to " + moves[2]);
             System.out.println("remis - punkt dla Komputera");
             compPoints += 1;
             gameResult();
-        } else if(compResult == 3 && move == 2){
+        } else if (compResult == 3 && input.equals("2")) {
             System.out.println("Ruch komputera to " + moves[2]);
             System.out.println("Twój ruch to " + moves[1]);
             System.out.println("Nożyce tną papier - punkt dla Komputera");
             compPoints += 1;
             gameResult();
-        } else if(compResult == 2 && move == 3){
+        } else if (compResult == 2 && input.equals("3")) {
             System.out.println("Ruch komputera to " + moves[1]);
             System.out.println("Twój ruch to " + moves[2]);
             System.out.println("Nożyce tną papier - punkt dla Gracza");
--- kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
@@ -1,54 +1,24 @@
 package com.kodilla.rps;
 
-import java.io.IOException;
-import java.security.KeyException;
-import java.util.InputMismatchException;
-import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
 
 public class RpsRunner {
 
-    public static void main(String[] args) throws IllegalArgumentException, InputMismatchException {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
         Player player1 = new Player();
         player1.getName();
         int maxScore = player1.getMaxScore();
-        //String nowaGra = player1.getNowaGra();
-        System.out.println("Instrukcja: 1-kamień, 2-papier, 3-nożyce, x-koniec gry, n-nowa gra");
 
-        boolean end = false;
-        while (!end) {
-            //end = koniec(scanner, end);
-
+        while (!player1.getEnd()) {
             player1.generateMove();
             if (player1.getPlayerPoints() == maxScore) {
                 player1.isItEnd();
-                end = true;
             } else if (player1.getCompPoints() == maxScore) {
                 player1.isItEnd();
-                end = true;
-            } else player1.isItEnd();
+            }
+            else player1.isItEnd();
         }
-    }
-
-    static boolean koniec(Scanner scanner, boolean end) {
-        if(Objects.equals(scanner.nextLine(), "x")){
-            System.out.println("Koniec gry? Jesteś pewien? tak/nie");
-            if(Objects.equals(scanner.nextLine(), "tak")){
-                end = true;
-            } else if(Objects.equals(scanner.nextLine(), "nie")){
-                System.out.println("Tak myślałem! Gramy dalej!");
-            } else System.out.println("wprowadź poprawne dane!");
-        } else if (Objects.equals(scanner.nextLine(), "n")) {
-            System.out.println("Czy chcesz przerwać rozgrywkę i zacząć nową grę?  tak/nie");
-            if(Objects.equals(scanner.nextLine(), "tak")){
-                end = true;
-            } else if(Objects.equals(scanner.nextLine(), "nie")){
-                System.out.println("Tak myślałem! Gramy dalej!");
-            } else System.out.println("wprowadź poprawne dane!");
-        } else System.out.println("Brak funkcji dla tego klawisza");
-        return end;
     }
 }
 
